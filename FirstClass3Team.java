7a4444c56940855869b9bd423b9f3c2ded199162
Richard-TGlu <48039672+Richard-TGlu@users.noreply.github.com>
2024-05-08 15:58:07 +0800

    修正骨幹錯誤

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -3,7 +3,7 @@ public class FirstClass3Team
     public static void main(String[] args)
     {
         System.out.println("這是用來練習GitHub協作的程式!");
-        System.out.println("\學號:\"01081022\"\n姓名:\"林韋全\"");
-        System.out.println("\學號:\"12345678\"\n姓名:\"谷翔平\"");
+        System.out.println("\"學號:\"01081022\"\n姓名:\"林韋全\"");
+        System.out.println("\"學號:\"12345678\"\n姓名:\"谷翔平\"");
     }
 }
