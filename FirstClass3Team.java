d8a29f6f710b6f9a5f814c55bb48212fd1974a9a
Qiiize <junqi525fizz@gmail.com>
2024-05-08 21:53:24 +0800

    Update FirstClass3Team.java

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -4,6 +4,7 @@ public class FirstClass3Team
     {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號:\"01081022\"\n姓名:\"林韋全\"");
+        System.out.println("\"學號:\"01157055\"\n姓名:\"蔡竣頎\"");
         System.out.println("\"學號:\"12345678\"\n姓名:\"谷翔平\"");
     }
 }
