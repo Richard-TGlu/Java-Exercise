b232d7b48699747a8b58066534af8206fc4bf63e
Richard-TGlu <48039672+Richard-TGlu@users.noreply.github.com>
2024-05-08 15:51:26 +0800

    feat:設定程式骨幹

---
+++ FirstClass3Team.java
@@ -0,0 +1,8 @@
+public class FirstClass3Team
+{
+    public static void main(String[] args)
+    {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\學號:\"12345678\"\n姓名:\"谷翔平\"");
+    }
+}
