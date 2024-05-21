95c94614b7946b0b6836b7f6431ef5dbbecd0344
Qiiize <junqi525fizz@gmail.com>
2024-05-22 00:02:42 +0800

    Player and PlayerDAO

---
+++ Player.java
@@ -0,0 +1,104 @@
+public class Player {
+    private int player_id;
+    private int team_id;
+    private String name;
+    private int number;
+    private Position position;
+    private int age;
+    private int height;
+    private int weight;
+    
+    public enum Position {PG,SG,SF,PF,C};
+
+    public Player(){
+    }
+
+    public int getPlayerID(){
+        return this.player_id;
+    }
+    public void setPlayerID(int player_id){
+        this.player_id = player_id;
+    }
+    
+    public int getTeamID(){
+        return this.team_id;
+    }
+    public void setTeamID(int team_id){
+        this.team_id = team_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public Position getPosition(){
+        return this.position;
+    }
+    
+    public void setPosition(String positionCode){
+        switch (positionCode) {
+            case "PG":
+                this.position = Position.PG;
+                break;
+            case "SG":
+                this.position = Position.SG;
+                break;
+            case "SF":
+                this.position = Position.SF;
+                break;
+            case "PF":
+                this.position = Position.PF;
+                break;
+            case "C":
+                this.position = Position.C;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getHeight(){
+        return this.height;
+    }
+    public void setHeight(int height){
+        this.height = height;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "player_id=" + player_id +
+                ", team_id=" + team_id +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                ", position=" + position +
+                ", age=" + age +
+                ", height=" + height +
+                "cm, weight=" + weight +
+                "kg}";
+}
+}
---
+++ PlayerDAO.java
@@ -0,0 +1,63 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class PlayerDAO {
+    private static final String url = "jdbc:mysql://34.80.246.158:3306/basketball?serverTimezone=UTC";
+    private static final String user = "CSEbasketball"; 
+    private static final String password = "junqi525";
+
+    public void addPlayer(String teamName, String name, int number, Player.Position position, int age, int height, int weight){
+        try (Connection conn = DriverManager.getConnection(url, user, password)){
+            TeamDAO teamDAO = new TeamDAO();
+            int team_id = teamDAO.getTeamIdByName(teamName);
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO Players (team_id, name, number, position, age, height, weight) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            stmt.setInt(1, team_id);
+            stmt.setString(2, name);
+            stmt.setInt(3, number);
+            stmt.setString(4, position.name());
+            stmt.setInt(5, age);
+            stmt.setInt(6, height);
+            stmt.setInt(7, weight);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<Player> getTeamPlayers(int team_id){
+        ArrayList<Player> playerList = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Players WHERE team_id = ?")) {
+            stmt.setInt(1, team_id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Player player = new Player();
+                player.setPlayerID(rs.getInt("player_id"));
+                player.setTeamID(rs.getInt("team_id"));
+                player.setName(rs.getString("name"));
+                player.setNumber(rs.getInt("number"));
+                String positionCode = rs.getString("position");
+                player.setPosition(positionCode);
+                player.setAge(rs.getInt("age"));
+                player.setHeight(rs.getInt("height"));
+                player.setWeight(rs.getInt("weight"));
+                playerList.add(player);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return playerList;
+    }
+
+    public static void main(String[] args){
+        TeamDAO teamDAO = new TeamDAO();
+        PlayerDAO playerDAO = new PlayerDAO();
+        ArrayList<Player> playerList;
+        //playerDAO.addPlayer("Lakers", "Lebron James", 23, Player.Position.PF, 39, 206, 113);
+        //playerDAO.addPlayer("Lakers", "Anthony Davis", 3, Player.Position.C, 31, 208, 115);
+        playerList = playerDAO.getTeamPlayers(teamDAO.getTeamIdByName("Lakers"));
+        for(Player player : playerList){
+            System.out.println(player.toString());
+        }
+    }
+}
