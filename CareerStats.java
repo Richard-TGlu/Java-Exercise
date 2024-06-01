c54bd55c63387f3683465686ede9e45ba4f1c9ea
Qiiize <junqi525fizz@gmail.com>
2024-06-01 21:45:36 +0800

    Add files via upload

---
+++ CareerStats.java
@@ -0,0 +1,143 @@
+public class CareerStats {
+    private String playerName;
+    
+    private double playingTime;
+    private double points;
+    private double assists;
+    private double rebounds;
+    private double steals;
+    private double blocks;
+    private double turnovers;
+    private double foul;
+    private double FGM;
+    private double FGA;
+    private double threePM;
+    private double threePA;
+    private double FTM;
+    private double FTA;
+
+    // Constructor
+    public CareerStats() {
+    }
+
+    // Getters and Setters
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public double getPlayingTime() {
+        return playingTime;
+    }
+
+    public void setPlayingTime(double playingTime) {
+        this.playingTime = playingTime;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public void setPoints(double points) {
+        this.points = points;
+    }
+
+    public double getAssists() {
+        return assists;
+    }
+
+    public void setAssists(double assists) {
+        this.assists = assists;
+    }
+
+    public double getRebounds() {
+        return rebounds;
+    }
+
+    public void setRebounds(double rebounds) {
+        this.rebounds = rebounds;
+    }
+
+    public double getSteals() {
+        return steals;
+    }
+
+    public void setSteals(double steals) {
+        this.steals = steals;
+    }
+
+    public double getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(double blocks) {
+        this.blocks = blocks;
+    }
+
+    public double getTurnovers() {
+        return turnovers;
+    }
+
+    public void setTurnovers(double turnovers) {
+        this.turnovers = turnovers;
+    }
+
+    public double getFoul() {
+        return foul;
+    }
+
+    public void setFoul(double foul) {
+        this.foul = foul;
+    }
+
+    public double getFGM() {
+        return FGM;
+    }
+
+    public void setFGM(double FGM) {
+        this.FGM = FGM;
+    }
+
+    public double getFGA() {
+        return FGA;
+    }
+
+    public void setFGA(double FGA) {
+        this.FGA = FGA;
+    }
+
+    public double getThreePM() {
+        return threePM;
+    }
+
+    public void setThreePM(double threePM) {
+        this.threePM = threePM;
+    }
+
+    public double getThreePA() {
+        return threePA;
+    }
+
+    public void setThreePA(double threePA) {
+        this.threePA = threePA;
+    }
+
+    public double getFTM() {
+        return FTM;
+    }
+
+    public void setFTM(double FTM) {
+        this.FTM = FTM;
+    }
+
+    public double getFTA() {
+        return FTA;
+    }
+
+    public void setFTA(double FTA) {
+        this.FTA = FTA;
+    }
+}
